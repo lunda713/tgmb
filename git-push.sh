ee7ea35d90cf372051a1ddce150175f176f68d88
akilarlxh <akilarlxh@gmail.com>
2020-08-04 11:42:32 +0800

    deploy from hexo-admin

--- git-push.sh
+++ git-push.sh
@@ -1,4 +1,3 @@
-git remote set-url origin https://github.com/Akilarlxh/Akilarlxh.github.io.git
 git add .
 git commit -m "deploy from hexo-admin"
 git push origin source
---
+++ hexo-install.sh
@@ -0,0 +1,5 @@
+git remote set-url origin https://github.com/Akilarlxh/Akilarlxh.github.io.git
+npm config set registry http://registry.npm.taobao.org
+npm install -g hexo-cli
+npm install
+npm install hexo-deployer-git
