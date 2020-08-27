5bdaa9fd56786111ad115c043e1569a49d8bf0db
akilarlxh <akilarlxh@gmail.com>
2020-08-27 17:31:42 +0800

    Update posts' content

---
+++ themes/butterfly/source/js/redirect.js
@@ -0,0 +1,17 @@
+<script src="http://cdn.bootcss.com/purl/2.3.1/purl.min.js"></script>
+
+<script>
+var url = purl();
+if (url.attr('host') == 'akilarlxh.github.io') {
+    var old_url = url.attr('source');
+    var new_url = old_url.replace('akilarlxh.github.io', "akilar.top");
+    window.location.replace(new_url);
+}else if (url.attr('host') == 'akilarlxh.github.io') {
+    var old_url = url.attr('source');
+    var new_url = old_url.replace('akilarlxh.github.io', "akilar.top");
+    window.location.replace(new_url);
+}else {
+    window.location.href="https://akilar.top";
+}
+
+</script>
