2588658d55ca0427b1a092317608ab77d79feb14
akilarlxh <akilarlxh@gmail.com>
2020-09-23 05:50:26 +0000

    Site updated: 2020-09-23 05:50:24

--- js/dytitle.js
+++ js/dytitle.js
@@ -2,12 +2,12 @@ var OriginTitile = document.title;
 var titleTime;
 document.addEventListener('visibilitychange', function () {
     if (document.hidden) {
-        $('[rel="shortcut icon"]').attr('href', "/img/siteicon/144.png");
+        $('[rel="shortcut icon"]').attr('href', "/img/siteicon/favicon.png");
         document.title = 'w(ﾟДﾟ)w 不要走！再看看嘛！';
         clearTimeout(titleTime);
     }
     else {
-        $('[rel="shortcut icon"]').attr('href', "/img/siteicon/144.png");
+        $('[rel="shortcut icon"]').attr('href', "/img/siteicon/favicon.png");
         document.title = '♪(^∇^*)欢迎回来！' + OriginTitile;
         titleTime = setTimeout(function () {
             document.title = OriginTitile;
