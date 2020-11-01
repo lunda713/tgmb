4dea37ce200e4fc2a729b07f75ab7da28513e445
akilarlxh <akilarlxh@gmail.com>
2020-11-01 15:08:46 +0000

    Site updated: 2020-11-01 15:08:44

--- galmenu/galmenu.js
+++ galmenu/galmenu.js
@@ -1,1 +1,144 @@
-"use strict";function _typeof(e){return(_typeof="function"==typeof Symbol&&"symbol"==typeof Symbol.iterator?function(e){return typeof e}:function(e){return e&&"function"==typeof Symbol&&e.constructor===Symbol&&e!==Symbol.prototype?"symbol":typeof e})(e)}for(var items=document.querySelectorAll(".menuItem"),i=0,l=items.length;i<l;i++)items[i].style.left=(50-35*Math.cos(-.5*Math.PI-1/l*2*i*Math.PI)).toFixed(4)+"%",items[i].style.top=(50+35*Math.sin(-.5*Math.PI-1/l*2*i*Math.PI)).toFixed(4)+"%";!function(r){var a={defaults:{click_to_close:!0,stay_open:!1},init:function(e){var n=e;r(this).each(function(e){var t=r(this),o=r.extend({},a.defaults,n),i=r("."+o.menu);t.on("mousedown",function(e){3!==e.which&&r(e.target).parents(".GalMenu").length<1&&o.click_to_close&&(t.find(".GalMenu").stop(!0,!1).animate({opacity:0},{duration:100,queue:!1,complete:function(){r(this).css("display","none").find(".active").removeClass("active").next().stop(!0,!0).slideUp("normal")}}),r(".circle").removeClass("open"),r("#overlay").hide(),r(".GalMenu").delay(400).hide(0),audio.pause(),audio.currentTime=0)}),t.on("contextmenu",function(e){e.preventDefault(),e.stopPropagation(),a.getCoords(e),r(".GalMenu_close_me").stop(!0,!1).animate({opacity:0},{duration:100,queue:!1,complete:function(){r(this).css("display","none")}});var t=r("#audio")[0],o=Coords.clientY-150,n=r("body")[0]===e.target?Coords.clickX-150:Coords.clientX-150,c=document.documentElement.clientHeight,l=document.documentElement.clientWidth;o<0&&(o=0),c-Coords.clientY<150&&(o=c-300),n<0&&(n=0),r("body")[0]===e.target?l-Coords.clickX<150&&(n=l-300):l-Coords.clientX<150&&(n=l-300),i.css({top:o+"px",left:n+"px",display:"block"}).stop(!0,!1).animate({opacity:1},{duration:100,queue:!1}),r("#gal").hasClass("open")?(r(".circle").removeClass("open"),r("#overlay").hide(),r(".GalMenu").delay(400).hide(0),t.pause(),t.currentTime=0):(r(".circle").addClass("open"),r("#overlay,.GalMenu").show(),t.play())})})},getCoords:function(e){var t=e||window.event,o=0,n=0;return(t.clientX||t.clientY)&&document.body&&null!=document.body.scrollLeft&&(o=t.clientX+document.body.scrollLeft,n=t.clientY+document.body.scrollTop),(t.clientX||t.clientY)&&"CSS1Compat"==document.compatMode&&document.documentElement&&null!=document.documentElement.scrollLeft&&(o=t.clientX+document.documentElement.scrollLeft,n=t.clientY+document.documentElement.scrollTop),(t.pageX||t.pageY)&&(o=t.pageX,n=t.pageY),Coords={clickX:o,clickY:n,clientX:t.clientX,clientY:t.clientY,screenX:t.screenX,screenY:t.screenY}}};r.fn.GalMenu=function(e,t){return a[e]?a[e].apply(this,Array.prototype.slice.call(arguments,1)):"object"!==_typeof(e)&&e?void r.error("Method "+e+" does not exist"):a.init.apply(this,arguments)}}(jQuery),String.prototype.removeWS=function(){return this.toString().replace(/\s/g,"")},String.prototype.pF=function(){return parseFloat(this)},Number.prototype.pF=function(){return parseFloat(this)},$(document).ready(function(){$("body").GalMenu({menu:"GalDropDown"})});
+var items = document.querySelectorAll('.menuItem');
+for (var i = 0, l = items.length; i < l; i++) {
+  items[i].style.left = (50 - 35 * Math.cos(-0.5 * Math.PI - 2 * (1 / l) * i * Math.PI)).toFixed(4) + "%";
+  items[i].style.top = (50 + 35 * Math.sin(-0.5 * Math.PI - 2 * (1 / l) * i * Math.PI)).toFixed(4) + "%"
+}
+
+
+(function($) {
+    var GalMenu = {
+        defaults: {
+            click_to_close: true,
+            stay_open: false
+        },
+        init: function(options) {
+            var o = options,
+            $this = $(this);
+            $this.each(function(i) {
+                var $this = $(this),
+                settings = $.extend({},
+                GalMenu.defaults, o),
+                $menu = $('.' + settings.menu);
+                $this.on('mousedown',
+                function(e) {
+                    if (e.which !== 3 && $(e.target).parents('.GalMenu').length < 1 && settings.click_to_close) {
+                        $this.find('.GalMenu').stop(true, false).animate({
+                            opacity: 0
+                        },
+                        {
+                            duration: 100,
+                            queue: false,
+                            complete: function() {
+                                $(this).css('display', 'none').find('.active').removeClass('active').next().stop(true, true).slideUp('normal')
+                            }
+                        });
+                        $(".circle").removeClass("open");
+                        $("#overlay").hide();
+                        $(".GalMenu").delay(400).hide(0);
+                        audio.pause();
+                        audio.currentTime = 0
+                    }
+                });
+                $this.on('contextmenu',
+                function(e) {
+                    e.preventDefault();
+                    e.stopPropagation();
+                    GalMenu.getCoords(e);
+                    $('.GalMenu_close_me').stop(true, false).animate({
+                        opacity: 0
+                    },
+                    {
+                        duration: 100,
+                        queue: false,
+                        complete: function() {
+                            $(this).css('display', 'none')
+                        }
+                    });
+                    var audio = $("#audio")[0];
+                    var add = 150;
+                    var top = Coords.clientY - add,
+                    left = ($('body')[0] === e.target) ? Coords.clickX - add: Coords.clientX - add;
+					//防止菜单超出可见区域，不需要可注释掉
+                    var bodyHe= document.documentElement.clientHeight;
+                    var bodyWi = document.documentElement.clientWidth;
+                    if(top<0) top=0;
+                    if(bodyHe-Coords.clientY<150) top=bodyHe-300;
+                    if(left<0) left=0;
+                    if($('body')[0] === e.target){if(bodyWi-Coords.clickX<150) left=bodyWi-300} else {if(bodyWi-Coords.clientX<150) left=bodyWi-300};
+					//防止菜单超出可见区域，不需要可注释掉
+                    $menu.css({
+                        top: top + 'px',
+                        left: left + 'px',
+                        display: 'block'
+                    }).stop(true, false).animate({
+                        opacity: 1
+                    },
+                    {
+                        duration: 100,
+                        queue: false
+                    });
+                    if ($("#gal").hasClass("open")) {
+                        $(".circle").removeClass("open");
+                        $("#overlay").hide();
+                        $(".GalMenu").delay(400).hide(0);
+                        audio.pause();
+                        audio.currentTime = 0
+                    } else {
+                        $(".circle").addClass("open");
+                        $("#overlay,.GalMenu").show();
+                        audio.play()
+                    }
+                })
+            })
+        },
+        getCoords: function(e) {
+            var evt = e ? e: window.event;
+            var clickX = 0,
+            clickY = 0;
+            if ((evt.clientX || evt.clientY) && document.body && document.body.scrollLeft != null) {
+                clickX = evt.clientX + document.body.scrollLeft;
+                clickY = evt.clientY + document.body.scrollTop
+            };
+            if ((evt.clientX || evt.clientY) && document.compatMode == 'CSS1Compat' && document.documentElement && document.documentElement.scrollLeft != null) {
+                clickX = evt.clientX + document.documentElement.scrollLeft;
+                clickY = evt.clientY + document.documentElement.scrollTop
+            };
+            if (evt.pageX || evt.pageY) {
+                clickX = evt.pageX;
+                clickY = evt.pageY
+            };
+            return Coords = {
+                clickX: clickX,
+                clickY: clickY,
+                clientX: evt.clientX,
+                clientY: evt.clientY,
+                screenX: evt.screenX,
+                screenY: evt.screenY
+            }
+        }
+    };
+    $.fn.GalMenu = function(method, options) {
+        if (GalMenu[method]) {
+            return GalMenu[method].apply(this, Array.prototype.slice.call(arguments, 1))
+        } else if (typeof method === 'object' || !method) {
+            return GalMenu.init.apply(this, arguments)
+        } else {
+            $.error('Method ' + method + ' does not exist')
+        }
+    }
+})(jQuery);
+String.prototype.removeWS = function() {
+    return this.toString().replace(/\s/g, '')
+};
+String.prototype.pF = function() {
+    return parseFloat(this)
+};
+Number.prototype.pF = function() {
+    return parseFloat(this)
+};
+
+$(document).ready(function() {
+  $('body').GalMenu({
+    'menu': 'GalDropDown'
+  })
+});
